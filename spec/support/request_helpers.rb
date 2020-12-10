d07607bf4dfeccd206d8430dbea92a917b9a0fe6
Nikolai Vasilenko <colormailbox@gmail.com>
2020-12-10 08:06:46 +0300

    Fix typo

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,9 +1,9 @@
 module RequestHelpers
   module Json
     def response_body
-      @response_body = nil if @reponse_object_id != response.object_id
+      @response_body = nil if @response_object_id != response.object_id
 
-      @reponse_object_id = response.object_id
+      @response_object_id = response.object_id
       @response_body ||= JSON.parse(response.body, symbolize_names: true)
     end
   end
